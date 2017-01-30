aa6e007409653ac58b0c0a900e7fe5f13cb988d0
Page David <david991010@gmail.com>
2017-01-30 19:26:05 +0800

    add shorter reverse tan algo, which may perform better

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env sage
 import Gauss_Legendre_mpmath
 import reverse_tan
+import reverse_tan_John_Machin
 import pi_compare
 import unbounded_spigot
 import spigot
+import Chudnovsky
 import time
 from sage.all import *
 import sys
@@ -29,15 +31,15 @@ class Analyser(object):
 				self.time_set.append((d, end_time))
 				accuracy = pi_compare.compare(res)[0]
 				self.accuracy_list.append(accuracy)
-				print d, end_time, accuracy
+				print m.name, d, end_time, accuracy
 				if end_time > 0.1: break
 			self.figure += list_plot(self.time_set, color = m.color, legend_label = m.name)
 			self.figure2 += list_plot(self.accuracy_list, color = m.color)
 			self.time_set, self.accuracy_list = list(), list()
 		self.figure.axes_labels(["$digits$", "$time$"])
 		self.figure2.axes_labels(["$digits$", "$accurancy$"])
-		save(self.figure.plot(), filename="time.svg", figsize=10, ymax=0.1)
-		save(self.figure2.plot(), filename="accurancy.svg")
+		save(self.figure.plot(), filename="time.svg", figsize=10, ymax=0.1, xmax=1000)
+		save(self.figure2.plot(), filename="accurancy.svg", ymin=0.9, ymax=1.0)
 
 class Pi_Func(object):
 
@@ -52,7 +54,9 @@ if __name__ == "__main__":
 				Pi_Func("Gauss_Legendre", "red", Gauss_Legendre_mpmath.pi),
 				Pi_Func("Reverse_Tan", "blue", reverse_tan.pi),
 				Pi_Func("Jeremy Gibbons's Unbounded Spigot", "green", unbounded_spigot.pi),
-				Pi_Func("Stanley Rabinowitz and Stan Wagon's spigot algorithm", "orange", spigot.pi)
+				Pi_Func("Stanley Rabinowitz and Stan Wagon's spigot algorithm", "orange", spigot.pi),
+				Pi_Func("Chudnovsky algorithm", "black", Chudnovsky.pi),
+				Pi_Func("Reverse_Tan_John_Machin", "yellow", reverse_tan_John_Machin.pi)
 				]
 	analyse = Analyser(method_list, sys.argv[1])
 	analyse.run()
