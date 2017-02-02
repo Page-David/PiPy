ea768d61954c90d96e4b6ac05a6c44534bec8cab
Page David <david991010@gmail.com>
2017-02-03 07:56:31 +0800

    replace for with while

--- Chudnovsky.py
+++ Chudnovsky.py
@@ -6,20 +6,18 @@ import pi_compare
 def pi(digits):
 	mp.dps = digits
 	K, M, L, X, S = 6, mpf('1'), 13591409, 1, mpf('13591409')
-	new_S = S
-	for i in xrange(0,1000000):
+	new_S = i = 0
+	while new_S != S:
+		new_S = S
 		M = (K**3 - K*16) * M / (i+1)**3
 		L += 545140134
 		X *= -262537412640768000
-		new_S += (M * L) / X
+		S += (M * L) / X
 		K += 12
-		if new_S == S:
-			break
-		else:
-			S = new_S
+		i += 1
 	return str(426880 * mpf('10005').sqrt() / S)
 
 if __name__ == "__main__":
-	P = pi()
+	P = pi(1000)
 	print P
 	print pi_compare.compare(str(P))
