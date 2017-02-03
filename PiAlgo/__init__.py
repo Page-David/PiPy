9c67a0324c0e87536181feca5a4b53fa7702e19e
Page David <david991010@gmail.com>
2017-02-03 12:00:29 +0800

    module init file update

--- PiAlgo/__init__.py
+++ PiAlgo/__init__.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-import os
-import Gauss_Legendre_mpmath
-import reverse_tan
-import reverse_tan_John_Machin
-import reverse_tan_Euler
-import pi_compare
-import unbounded_spigot
-import spigot
-import Chudnovsky
-
 from os.path import dirname, basename, isfile
 import glob
 
